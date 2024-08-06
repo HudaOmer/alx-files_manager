f98d6d3ccb8c66793d0faa4d51c82e50823ba3d5
HudaOmer <hudaoe175@gmail.com>
2024-08-06 14:36:19 -0700

    Solve Task 3

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,14 @@
 import { Router } from 'express';
 import AppController from '../controllers/AppController';
+import UsersController from '../controllers/UsersController';
 // 2. First API: status stats
+// 3. Create a new user: users
 
 const router = Router();
 
 router.get('/status', AppController.getStatus);
 router.get('/stats', AppController.getStats);
+
+router.post('/users', UsersController.postNew);
 
 module.exports = router;
