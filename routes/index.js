567dbc2113901ae8c516333b341451a0e003b382
HudaOmer <hudaoe175@gmail.com>
2024-08-06 15:04:50 -0700

    Solve Task 5

--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,11 @@ import { Router } from 'express';
 import AppController from '../controllers/AppController';
 import UsersController from '../controllers/UsersController';
 import AuthController from '../controllers/AuthController';
+import FilesController from '../controllers/FilesController';
 // 2. First API: status stats
 // 3. Create a new user: users
 // 4. Authenticate a user: connect disconnect user/me
+// 5. First file: 
 
 const router = Router();
 
@@ -16,5 +18,7 @@ router.post('/users', UsersController.postNew);
 router.get('/connect', AuthController.getConnect);
 router.get('/disconnect', AuthController.getDisconnect);
 router.get('/users/me', UsersController.getMe);
+
+router.post('/files', FilesController.postUpload);
 
 module.exports = router;
