8bbe3ea0b1b061b52786cdbe5e3f34a8b0620626
HudaOmer <hudaoe175@gmail.com>
2024-08-06 14:24:46 -0700

    Solve Task 2

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,10 @@
 import { Router } from 'express';
 import AppController from '../controllers/AppController';
-// 2. First API one two
+// 2. First API: status stats
 
 const router = Router();
 
 router.get('/status', AppController.getStatus);
 router.get('/stats', AppController.getStats);
 
-module.exports = router
+module.exports = router;
